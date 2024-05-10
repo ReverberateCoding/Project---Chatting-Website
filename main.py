e27d775367b6731ca2938f3e196e93f91559f0df
reverberate <raphaellimfuencoding@gmail.com>
2024-05-11 00:57:02 +0800

    AHHH

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from wtform_field import *
-from models import *
 
 app = Flask(__name__)
 
@@ -11,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+from models import *
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -19,14 +19,11 @@ def index():
         username_data = reg_form.username.data
         password_data = reg_form.password.data
 
-        user_object = User.query.filter_by(username=username_data).first()
-        if user_object:
-            return "Someone else has taken this username!"
-        else:
-            user = User(username=username_data, password=password_data)
-            db.session.add(user)
-            db.session.commit()
-            return "Inserted into DB!"
+       
+        user = User(username=username_data, password=password_data)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into DB!"
     return render_template('index.html',form=reg_form)
 
 if __name__ == "__main__":
