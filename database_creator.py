cd998d2978e0c661d658d040a9038c0f222ef76e
reverberate <raphaellimfuencoding@gmail.com>
2024-05-11 00:37:14 +0800

    Commit 1

---
+++ database_creator.py
@@ -0,0 +1,3 @@
+from main import app, db
+with app.app_context():
+	db.create_all()
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template, redirect, url_for
 from wtform_field import *
+from models import *
 
 app = Flask(__name__)
+
 app.secret_key = 'replace later'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "Successfully submitted!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Inserted into DB!"
     return render_template('index.html',form=reg_form)
 
 if __name__ == "__main__":
