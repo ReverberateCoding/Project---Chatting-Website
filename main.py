fd451a6e40d27d8c7c1c8d297a1f6c74ef08ec4f
Raphael Lim <121223374+ReverberateCoding@users.noreply.github.com>
2024-05-10 12:41:36 +0000

    Added flask_sqlalchemy

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
+from wtform_field import *
 
 app = Flask(__name__)
+app.secret_key = 'replace later'
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Successfully submitted!"
+    return render_template('index.html',form=reg_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ wtform_field.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username_label',validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+    confirm_password = PasswordField('confirm_password_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Password must match")])
+    submit_button = SubmitField("Create")
+    
