f6568437f7ed4b258a88a3748ba9c635e519caf1
reverberate <raphaellimfuencoding@gmail.com>
2024-05-11 16:47:48 +0800

    add a login and registration page

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, request
-from wtform_field import RegistrationForm
+from flask import render_template, request, redirect, url_for
+from wtform_field import RegistrationForm, LoginForm
 from models import User
 
 def register_routes(app, db):
@@ -9,11 +9,17 @@ def register_routes(app, db):
         if reg_form.validate_on_submit():
             username_data = reg_form.username.data
             password_data = reg_form.password.data
-            if User.query.filter_by(username=username_data).first():
-                return "Someone else has taken this username!"
             user = User(username=username_data, password=password_data)
             print(user)
             db.session.add(user)
             db.session.commit()
+            return redirect(url_for('login'))
         users = User.query.all()
         return render_template('index.html',form=reg_form, users=users)
+    
+    @app.route("/login", methods=['GET', 'POST'])
+    def login():
+        login_form = LoginForm()
+        if login_form.validate_on_submit():
+            return "Logged in!"
+        return render_template("login.html", form=login_form)
--- wtform_field.py
+++ wtform_field.py
@@ -1,10 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from models import User
+
+def InvalidCredentials(form, field): #WTForm automatically passes in arguements
+    username_entered = form.username.data
+    password_entered = form.password.data
+
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError(message="Username or password is incorrect")
+    elif password_entered != user_object.password:
+        raise ValidationError(message="Username or password is incorrect")
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('username_label',validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
     password = PasswordField('password_label', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_password = PasswordField('confirm_password_label', validators=[InputRequired(message="Password required"), EqualTo('password', message="Password must match")])
     submit_button = SubmitField("Create")
-    
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError(message="Username already exists. Please select a different username")
+        
+class LoginForm(FlaskForm):
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required"), InvalidCredentials])
+    submit_button = SubmitField("Login")
